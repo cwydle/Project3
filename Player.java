919d42bf3a07194281942f2c4f8e832e94af64b3
cwydle <90412176+cwydle@users.noreply.github.com>
2021-12-17 05:13:55 -0500

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,20 @@
+public class Player {
+    private int lives;
+    
+    public Player() {
+      this.lives = 5;
+    }
+    
+    public Player(int live) {
+      this.lives = live;
+    }
+    
+    public int getLives() {
+      return this.lives;
+    }
+    
+    public void death() {
+      this.lives--;
+    }
+  }
+  
